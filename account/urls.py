6bc328433656480d4fad159b837636c82980a382
Amirmahdy Pourvahaby <amirmahdypourvahaby@gmail.com>
2024-08-05 16:29:06 +0330

    Account User
    customize User model

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,5 @@ from .views import *
 
 app_name = 'account'
 urlpatterns = [
-    path('login', log_in, name='login'),
-    path('logout', log_out, name='logout'),
-    path('register', sign_up, name='register'),
+
 ]
